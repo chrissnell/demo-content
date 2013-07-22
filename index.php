5b51b4e8160f4f66dcb9e9fb8cb3e20775ee3f55
Christopher Snell <chris.snell@gmail.com>
2013-07-22 16:37:24 -0500

    new bootstrapyness

--- index.php
+++ index.php
@@ -7,20 +7,42 @@
 </head>
 
 <body>
-<div class="wrapper">
-<h1>Welcome to the ECS DevOps Demo!</h1>
-<p>This web page is being served from      
-   <strong><?php
-          echo gethostname();
-    ?></strong>
-</p>
-<p>You are visiting from
-   <strong><?php
-          echo $_SERVER['REMOTE_ADDR'];
-    ?></strong>
-</p>
-<p></p>
-<p>Powered by Apache and PHP.</p>
-</body>
-</html>
+<div class="container">
+
+  <!-- Main hero unit for a primary marketing message or call to action -->
+  <div class="hero-unit">
+    <h1>Welcome to the ECS DevOps Demo!</h1>
+    <p>This is a simple two-tiered PHP app to demonstrate the power of Chef provisioning in the Rackspace Cloud</p>
+  </div>
+
+  <!-- Example row of columns -->
+  <div class="row">
+    <div class="span6">
+      <h2>About This Server...</h2>
+	  <p>This web page is being served from      
+	   <strong>
+	  	<?php
+	     echo gethostname();
+	     ?>
+	   </strong>
+	  </p>
+    </div>
+    <div class="span6">
+      <h2>About You</h2>
+      <p>You are visiting from      
+	   <strong>
+	  	<?php
+	     echo $_SERVER['REMOTE_ADDR'];
+	     ?>
+	   </strong>
+	  </p>
+   </div>
+  </div>
+      <hr>
+
+      <footer>
+        <p>&copy; Powered by Chef, Ubuntu Linux, Apache, PHP, and the Rackspace Cloud </p>
+      </footer>
+
+    </div> <!-- /container -->
 
